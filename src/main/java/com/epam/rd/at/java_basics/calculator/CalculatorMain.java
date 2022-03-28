3df047462faaf84293b2eb4ed0d3fa71cfe2d3ac
sglenar <lenar_sagidullin@epam.com>
2022-03-28 23:47:08 +0400

    Task Calculator was added (v0.1).

--- src/main/java/com/epam/rd/at/java_basics/calculator/CalculatorMain.java
+++ src/main/java/com/epam/rd/at/java_basics/calculator/CalculatorMain.java
@@ -1,9 +1,50 @@
 package com.epam.rd.at.java_basics.calculator;
 
+import java.util.Scanner;
+
 public class CalculatorMain {
 
     public static void main(String[] args) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        double result = 0;
+        String plus = "+";
+        String minus = "-";
+        String multipl = "*";
+        String divis = "/";
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number A:");
+        double numberA = Integer.parseInt(scan.next());
+        System.out.println("Choose operation:");
+        String operator = String.valueOf(scan.next().charAt(0));
+        System.out.println("Enter number B:");
+        double numberB = Integer.parseInt(scan.next());
+        if (operator.equals(plus) ) {
+            result = sum(numberA,numberB);
+        } else if (operator.equals(minus)) {
+            result = difference(numberA,numberB);
+        } else if (operator.equals(multipl)) {
+            result = multiplication(numberA,numberB);
+        } else if (operator.equals(divis)) {
+            result = division(numberA,numberB);
+        }
+        System.out.println("Result = " + result);
+
+
+
+
     }
 
+    private static double sum(double a, double b) {
+        return a+b;
+    }
+    private static double  difference (double a, double b) {
+        return a-b;
+    }
+    private static double multiplication (double a, double b) {
+        return a*b;
+    }
+    private static double division (double a, double b) {
+        return a/b;
+    }
 }
+
+

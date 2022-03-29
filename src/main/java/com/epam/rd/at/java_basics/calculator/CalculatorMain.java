b9b5ffe98e1b8b0e005055f3980e2481de205f0a
sglenar <lenar_sagidullin@epam.com>
2022-03-29 21:09:12 +0400

    Task Calculator was added (v0.5).

--- src/main/java/com/epam/rd/at/java_basics/calculator/CalculatorMain.java
+++ src/main/java/com/epam/rd/at/java_basics/calculator/CalculatorMain.java
@@ -1,22 +1,42 @@
 package com.epam.rd.at.java_basics.calculator;
 
+import javax.print.attribute.standard.PresentationDirection;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CalculatorMain {
 
     public static void main(String[] args) {
-        double result = 0;
         String plus = "+";
         String minus = "-";
         String multipl = "*";
         String divis = "/";
         Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to Calculator App!" +"\nTo start, enter number A, operation (+, -, * or /) and number B ");
         System.out.println("Enter number A:");
-        int numberA = Integer.parseInt(scan.next());
+        String stringA = scan.next();
+        int numberA;
+        if (isNumber(stringA)) {
+            numberA = Integer.parseInt(stringA);
+        } else {
+            return;
+        }
         System.out.println("Choose operation:");
         String operator = String.valueOf(scan.next());
+        if (!Arrays.asList("+", "-", "*", "/").contains(operator)) {
+            System.out.println("Error");
+            System.out.println("Incorrect operator");
+            return;
+        }
         System.out.println("Enter number B:");
-        int numberB = Integer.parseInt(scan.next());
+        String stringB = scan.next();
+        int numberB;
+        if (isNumber(stringB)) {
+            numberB = Integer.parseInt(stringB);
+        } else {
+            return;
+        }
+        double result = 0;
         if (operator.equals(plus) ) {
             result = sum(numberA,numberB);
         } else if (operator.equals(minus)) {
@@ -25,7 +45,9 @@ public class CalculatorMain {
             result = multiplication(numberA,numberB);
         } else if (operator.equals(divis)) {
             result = division(numberA,numberB);
+            if (numberB == 0) {
             return;
+            }
         }
         System.out.println("Result = " + result);
 
@@ -53,7 +75,16 @@ public class CalculatorMain {
         }
         return  result;
     }
-
+    private static boolean isNumber (String enteredNumber) {
+        try {
+            Integer.parseInt(enteredNumber);
+            return true;
+        } catch (NumberFormatException e) {
+            System.out.println("Error");
+            System.out.println("Entered symbol is not a number");
+            return false;
+        }
+    }
 }
 
 
